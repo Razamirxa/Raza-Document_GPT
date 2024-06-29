fc938c35292f2bae9b096c513bd8b5d63bc43376
Raza UL Mustafa <122047009+Razamirxa@users.noreply.github.com>
2024-06-30 01:20:34 +0500

    Update app.py

--- app.py
+++ app.py
@@ -84,7 +84,7 @@ st.markdown("""
 def main():
     load_dotenv()
 
-    st.markdown("<h1 style='text-align: center; color: #0073e6;'>Raza GPT</h1>", unsafe_allow_html=True)
+    st.markdown("<h1 style='text-align: center; color: #0073e6;'>Elevate Your Document Experience with RAG GPT and Conversational AI</h1>", unsafe_allow_html=True)
     st.markdown("<h3 style='text-align: center; color: #0073e6;'>Upload Your File Select your Model and ask any Question from file</h3>", unsafe_allow_html=True)
 
 
